79d91ad55a2c40c81d35d14594cfe2408e58b60a
S-N-Onika <sumsunnaharonika12@gmail.com>
2026-05-21 18:08:40 +0600

    Stats section for home page is build

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,28 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function MainLayout() {
+    const location = useLocation();
+
     return (
-        <div className="flex flex-col min-h-screen bg-[#FBF8F3] text-[#2E1A0F] font-sans antialiased">
+        <div className="flex flex-col min-h-screen bg-[#FBF8F3] text-[#2E1A0F] font-sans antialiased overflow-x-hidden">
             <ScrollRestoration />
             <Navbar />
+
             <main className="flex-grow">
-                <Outlet key={window.location.pathname} />
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={location.pathname}
+                        initial={{ opacity: 0, y: 15 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -15 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                    >
+                        <Outlet />
+                    </motion.div>
+                </AnimatePresence>
             </main>
 
             <Footer />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Stats from "../components/Stats";
 
 export default function Home() {
     return (
         <div >
             <Banner />
+            <Stats />
         </div>
     );
 }
