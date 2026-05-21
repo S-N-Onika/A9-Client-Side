36a5bc6f97a0738e723669e34f70ec49ba117f59
S-N-Onika <sumsunnaharonika12@gmail.com>
2026-05-21 19:54:34 +0600

    LatestRooms section added in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import Banner from "../components/Banner";
+import LatestRooms from "../components/LatestRooms";
 import Stats from "../components/Stats";
 import Events from "../components/Events";
 
 export default function Home() {
     return (
-        <div >
+        <div className="bg-[#FBF8F3] min-h-screen flex flex-col">
             <Banner />
-            <Stats />
-            <Events />
+
+            <div className="bg-white">
+                <LatestRooms />
+            </div>
+
+            <div className="bg-[#FBF8F3] border-t border-[#EADFC9]/70 py-10">
+                <Stats />
+            </div>
+
+            <div className="bg-white border-t border-[#EADFC9]/70">
+                <Events />
+            </div>
         </div>
     );
 }
